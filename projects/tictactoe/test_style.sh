e6d59752c9951c3cae1785b57aad5eb30b591f4d
Epoch Dellicious <E.Dellicious@localhost.localdomain>
2017-11-20 09:08:17 +0100

    submission script modif + numpy type int

--- projects/tictactoe/test_style.sh
+++ projects/tictactoe/test_style.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #XXX : Do not use or modify. See submission_test.sh
 
 flkout="`flake8 *.py`"
 #File should pass flake8 style
-if [ -n $flkout ]; then
+if [ -n "$flkout" ]; then
         echo "Source code style does not meet flake8 standards. Please ensure it does using flake8, you can use autopep8 to automatize a part of the work for you."
         echo "Usage of autopep8 : autopep8 --in-place --aggressive --aggressive yourfile.py"
         exit -1
