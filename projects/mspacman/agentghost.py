8684a2051d2e2d7f241bdcd0071f0b795d8cdab1
Epoch <epoch@localhost.localdomain>
2017-11-24 15:33:16 +0100

    template agents for students

---
+++ projects/mspacman/agentghost.py
@@ -0,0 +1,11 @@
+from pacman import Directions
+from game import Agent
+# XXX: You should complete this class for Step 2
+
+
+class Agentghost(Agent):
+    def __init__(self, index=0):
+        pass
+
+    def getAction(self, state):
+        return Directions.STOP
