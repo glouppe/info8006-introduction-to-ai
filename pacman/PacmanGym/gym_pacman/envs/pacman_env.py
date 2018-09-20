507cb62e134814971ebdac8b9301027e5177d791
Sam Ait <epoch@localhost.localdomain>
2018-09-20 11:28:04 +0200

    Optimization on step method

--- pacman/PacmanGym/gym_pacman/envs/pacman_env.py
+++ pacman/PacmanGym/gym_pacman/envs/pacman_env.py
@@ -30,7 +30,7 @@ PACMAN_ACTIONS = ['North', 'South', 'East', 'West', 'Stop']
 PACMAN_DIRECTIONS = ['North', 'South', 'East', 'West']
 ROTATION_ANGLES = [0, 180, 90, 270]
 
-MAX_EP_LENGTH = np.inf
+
 
 
 fdir = '/'.join(os.path.split(__file__)[:-1])
@@ -191,38 +191,7 @@ class PacmanEnv(gym.Env):
         return self.game.state
 
     def step(self):
-        # implement code here to take an action
-        if self.step_counter >= MAX_EP_LENGTH or self.done:
-            self.step_counter += 1
-            return np.zeros(self.observation_space.shape), 0.0, True, {
-                'past_loc': [self.location_history[-2]],
-                'curr_loc': [self.location_history[-1]],
-                'past_orientation': [[self.orientation_history[-2]]],
-                'curr_orientation': [[self.orientation_history[-1]]],
-                'illegal_move_counter': [self.illegal_move_counter],
-                'step_counter': [[self.step_counter]],
-                'ghost_positions': [self.ghostLocations],
-                'r': [self.cum_reward],
-                'l': [self.step_counter],
-                'ghost_in_frame': [self.ghostInFrame],
-                'episode': [{
-                    'r': self.cum_reward,
-                    'l': self.step_counter
-                }]
-            }
-
-        """
-        queue = Queue()
-        proc = Process(target=get_action_timeout_call, args=(self.pacman,queue,self.game.state))
-        proc.start()
-        try:
-            pacman_action = queue.get(timeout=self.timeout)
-            proc.join()
-        except:
-            pacman_action = self.previous_pacman_action
-            proc.terminate()
-
-        """
+     
         with stopit.ThreadingTimeout(self.timeout) as to_ctx_mgr:
             assert to_ctx_mgr.state == to_ctx_mgr.EXECUTING
             pacman_action = self.pacman.get_action(self.game.state)
@@ -240,51 +209,19 @@ class PacmanEnv(gym.Env):
 
         reward = self.game.step(pacman_action)
         self.cum_reward += reward
-        # reward shaping for illegal actions
 
         done = self.game.state.isWin() or self.game.state.isLose()
 
-        self.location = self.game.state.data.agentStates[0].getPosition()
-        self.location_history.append(self.location)
-        self.ghostLocations = [a.getPosition()
-                               for a in self.game.state.data.agentStates[1:]]
 
-        self.orientation = PACMAN_DIRECTIONS.index(
-            self.game.state.data.agentStates[0].getDirection())
-        self.orientation_history.append(self.orientation)
-
-        extent = (self.location[0] -
-                  1, self.location[1] -
-                  1), (self.location[0] +
-                       1, self.location[1] +
-                       1),
-        self.ghostInFrame = any([g[0] >= extent[0][0] and g[1] >= extent[0][1] and g[0]
-                                 <= extent[1][0] and g[1] <= extent[1][1] for g in self.ghostLocations])
-        self.step_counter += 1
-        info = {
-            'past_loc': [self.location_history[-2]],
-            'curr_loc': [self.location_history[-1]],
-            'past_orientation': [[self.orientation_history[-2]]],
-            'curr_orientation': [[self.orientation_history[-1]]],
-            'illegal_move_counter': [self.illegal_move_counter],
-            'step_counter': [[self.step_counter]],
-            'episode': [None],
-            'ghost_positions': [self.ghostLocations],
-            'ghost_in_frame': [self.ghostInFrame],
-        }
+      
         self.previous_pacman_action = pacman_action
 
-        if self.step_counter >= MAX_EP_LENGTH:
-            done = True
+     
 
         self.done = done
 
-        if self.done:  # only if done, send 'episode' info
-            info['episode'] = [{
-                'r': self.cum_reward,
-                'l': self.step_counter
-            }]
-        return self.game.state, reward, done, info
+    
+        return self.game.state, reward, done, None
 
     def get_action_meanings(self):
         return [PACMAN_ACTIONS[i] for i in self._action_set]
