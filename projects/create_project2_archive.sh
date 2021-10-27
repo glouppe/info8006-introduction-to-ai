0b695b207fb9de034ae90165ef9e0853e3d64622
Arnaud Delaunoy <a.delaunoy@uliege.be>
2021-10-27 15:08:32 +0200

    update projet2 archives

---
+++ projects/create_project2_archive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tar zcvf project2.tar.gz project2/*.py project2/pacman_module/*.py project2/pacman_module/layouts/* project2/template-project2.tex
+zip project2.zip project2/*.py project2/pacman_module/*.py project2/pacman_module/layouts/* project2/template-project2.tex
