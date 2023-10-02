f4da638f31f52f488db43de25a928897d374899d
François Rozet <francois.rozet@outlook.com>
2023-10-02 11:47:08 +0200

    Update exercises 1

--- exercises/compile.sh
+++ exercises/compile.sh
@@ -15,14 +15,9 @@ done
 rename 's/e(\d)/exercises-$1-solutions/' *.pdf
 mv *.pdf ../pdf
 
-cp common.sty backup.sty
-sed -i 's/\\BODY//g' common.sty
-
 for file in $FILES; do
-    pdflatex -halt-on-error -interaction=nonstopmode $file
+    NOSOL=1 pdflatex -halt-on-error -interaction=nonstopmode $file
 done
 
 rename 's/e/exercises-/' *.pdf
 mv *.pdf ../pdf
-
-mv backup.sty common.sty
